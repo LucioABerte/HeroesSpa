6a8dff7cdfad9c2f1eccc6ea09fa10f0bd19bcb2
lucio <lucioaberte@gmail.com>
2023-03-01 15:33:10 -0300

    context

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { AppRouter } from './router'
+import { AuthProvider } from './auth'
 
 const HeroesApp = () => {
   return (
-    <>
+    <AuthProvider>
+
       <AppRouter/>
-    </>
+      
+    </AuthProvider>
   )
 }
 
